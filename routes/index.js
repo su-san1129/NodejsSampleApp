00c0ee9ab74233e6022415341b1619702420cf26
EC2 Default User <ec2-user@ip-172-31-34-181.ap-northeast-1.compute.internal>
2019-08-10 13:31:12 +0000

    再作成

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var moment = require('moment'); // 追加
+var connection = require('../mysqlConnection'); // 追加
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var query = 'SELECT *, DATE_FORMAT(created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM boards';
+  connection.query(query, function(err, rows) {
+    console.log(rows);
+    res.render('index', { title: 'Express' });
+  });
+});
+
+router.post('/', function(req, res, next) {
+  var title = req.body.title;
+  var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+  var query = 'INSERT INTO boards (title, created_at) VALUES ("' + title + '", ' + '"' + createdAt + '")';
+  connection.query(query, function(err, rows) {
+    res.redirect('/');
+  });
+});
+
+module.exports = router;
